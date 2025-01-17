7f0aa73413fa17a852a4e851c58ee7e5b66a2c20
Heshan Navindu <kawhnkarachchi@std.appsc.sab.ac.lk>
2025-01-17 23:56:52 +0530

    up

--- karadana_teat_factory/lib/widgets/navbar.dart
+++ karadana_teat_factory/lib/widgets/navbar.dart
@@ -1,21 +1,25 @@
 import 'package:flutter/material.dart';
+import '../screens/home/homepage.dart';
+import '../screens/supplier/supplier.dart';
+import '../screens/auth/loginpage.dart';
+import '../screens/teaCollection/tea.dart';
 
 class NavBar extends StatefulWidget {
   const NavBar({Key? key}) : super(key: key);
 
   @override
-  _BottomNavBarState createState() => _BottomNavBarState();
+  _NavBarState createState() => _NavBarState();
 }
 
-class _BottomNavBarState extends State<NavBar> {
+class _NavBarState extends State<NavBar> {
   int _selectedIndex = 0;
 
   // List of pages for navigation
-  static const List<Widget> _pages = <Widget>[
-    Text('Home'),
-    Text('Weight'),
-    Text('Anomaly'),
-    Text('Settings'),
+  final List<Widget> _pages = <Widget>[
+    const Home(), // Replace with your HomePage
+    const Supplier(), // Replace with your SupplierPage
+    const LogIn(), // Replace with your LoginPage
+    const TeaCollection(), // Replace with your TeaPage
   ];
 
   void _onItemTapped(int index) {
@@ -26,30 +30,33 @@ class _BottomNavBarState extends State<NavBar> {
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      backgroundColor: const Color.fromARGB(185, 183, 255, 185),
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.line_weight),
-          label: 'Weight',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.search),
-          label: 'Anomaly',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Settings',
-        ),
-      ],
-      currentIndex: _selectedIndex,
-      selectedItemColor: Colors.blue,
-      onTap: _onItemTapped,
+    return Scaffold(
+      body: _pages[_selectedIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color.fromARGB(185, 183, 255, 185),
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group),
+            label: 'Supplier',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.login),
+            label: 'Login',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.terrain_rounded), // You can change the icon
+            label: 'Tea',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
